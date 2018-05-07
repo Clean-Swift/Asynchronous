d2e3fdf1816a4342d783050139f5e8fb1241a96f
Raymond Law <ray@clean-swift.com>
2018-05-07 18:28:51 -0400

    This project demonstrates two different approaches to handle asynchronous operations:
    - Completion Handler
    - VIP Cycle

---
+++ Asynchronous/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Asynchronous
+//
+//  Created by Raymond Law on 2/22/18.
+//  Copyright © 2018 Clean Swift LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
